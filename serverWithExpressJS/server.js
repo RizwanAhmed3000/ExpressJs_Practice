4b827dfe3d31ac9cc3d4afb7aa71ebf794bff98a
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-08-12 12:36:44 +0500

    CRUD operation apis Done

--- serverWithExpressJS/server.js
+++ serverWithExpressJS/server.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { json } from 'express'
 import fs from 'fs'
 
 const app = express();
@@ -9,6 +9,8 @@ const tourJason = fs.readFileSync('./tour.json', 'utf-8')
 const parsedJson = JSON.parse(tourJason);
 // console.log(parsedJson);
 
+app.use(express.json())
+
 app.get('/api/v1/tour', (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -17,10 +19,7 @@ app.get('/api/v1/tour', (req, res) => {
     })
 })
 app.get('/api/v1/tour/:id', (req, res) => {
-    // console.log(req.params)
     const reqParamIdInNumber = +(req.params.id)
-    // console.log(reqParamIdInNumber, '==>> reqParamIdInNumber')
-    // console.log(typeof reqParamIdInNumber, '==>>type of reqParamIdInNumber')
     if (reqParamIdInNumber > parsedJson.data.length) {
        return res.status(404).send(`Data not found`)
     }
@@ -31,9 +30,67 @@ app.get('/api/v1/tour/:id', (req, res) => {
         status: 'success',
         data: idFillteredData
     })
+})
+app.post('/api/v1/tour', (req, res) => {
+   console.log(parsedJson.data.length, '==>> data length')
+   console.log(req.body);
+    if (!req.body.destinationName || !req.body.country || !req.body.price || !req.body.tourCapacity || !req.body.departure || !req.body.arrival || !req.body.transport){
+        res.status(400).json({
+            status: 'failed',
+            data: 'missing fields'
+        })
+    } else{
+        const dataToStoreInDb = {
+            id: parsedJson.data.length + 1,
+            ...req.body
+        }
 
+        parsedJson.data.push(dataToStoreInDb)
+
+        fs.writeFile('./tour.json', JSON.stringify(parsedJson), ()=>{
+            res.status(200).json({
+                status: 'success',
+                data: "Data added successfully"
+            })
+        })
+    }
 
 })
+app.delete('/api/v1/tour/:id', (req, res) => {
+    // console.log('delete api working')
+    const fillterData = parsedJson.data.filter((tour)=> tour.id != +(req.params.id))
+    // console.log(fillterData)
+    parsedJson.data = fillterData
+    fs.writeFile('./tour.json', JSON.stringify(parsedJson), () => {
+        res.status(200).json({
+            status: 'success',
+            data: "Data deleted successfully"
+        })
+    })
+
+})
+app.put('/api/v1/tour/:id', (req, res) => {
+//    console.log("put api working");
+    const reqParamIdInNumber = +(req.params.id)
+    
+    let indexNumber;
+    parsedJson.data.forEach((tour, idx)=>{
+        if(tour.id == reqParamIdInNumber){
+            indexNumber = idx
+        }
+    })
+    // console.log(indexNumber, '==> indexNumber')
+    parsedJson.data.splice(indexNumber, 1, req.body)
+    // console.log(parsedJson.data)
+    fs.writeFile('./tour.json', JSON.stringify(parsedJson), () => {
+        res.status(200).json({
+            status: 'success',
+            data: "Data updated successfully"
+        })
+    })
+
+})
+
 
 app.listen(port, () => {
     console.log(`listening to the server at port number: ${port}`)
